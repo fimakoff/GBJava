e021af101f37dc70a731f2f2f48cd45ad67f7cc5
fimakoff <fimakoff@gmail.com>
2019-03-12 18:00:42 +0300

    Add homework & extra homework lesson 5

--- src/lesson_5/Forestman.java
+++ src/lesson_5/Forestman.java
@@ -1,4 +1,38 @@
 package lesson_5;
 
+import java.util.Arrays;
+
 public class Forestman {
+    public static void main(String[] args) {
+        String mass = "5 1 5 2 2 4 1 4 5 1 5 3 2 4 4 4 5 1 3 4 2 2 1 2 4 4 4 5 4 3 5 4 4 5 5 1 4 1 5 3 1 4 5 3 3 4 2 2 4 4 5 5 1 1 1 4 5 5" +
+                " 4 4 2 4 3 1 3 3 1 1 3 1 3 4 4 3 2 2 1 3 4 4 2 3 4 2 4 4 1 4 4 4 2 1 2 4 1 5 2 2 5 4 2 2 3 1 5 5 3 5 3 1 4 5 4 2 1 3" +
+                " 1 2 1 4 1 3 4 2 2 5 2 3 1 1 2 3 3 4 4 2 4 1 2 2 2 5 1 5 1 2 2 1 3 3 4 3 5 3 5 1 2 1 3 3 2 4 1 4 3 5 1 2 1 2 3 2 1 3" +
+                " 2 2 4 3 2 1 5 1 4 5 4 4 5 5 4 2 3 5 1 3 4 3 2 4 5 2 5 2 4 1 4 5 2 3 3 4 4 3 5 2 2 3 5 1 2 4 3 4 4 3 2 2 1 4 5 5 1 5" +
+                " 2 4 5 5 4 2 2 1 5 1 3 4 2 4 2 2 4 3 5 2 2 4 4 4 5 5 2 5 5 2 5 1 1 5 5 4 1 2 4 1 2 2 5 4 5 1 5 4 4 5 5 5 3 3 4 3 3 5" +
+                " 3 2 2 2 2 2 1 2 5 2 3 4 3 5 5 2 4 5 3 4 3 1 3 2 1 1 5 4 4 2 3 1 3 4 2 4 1 3 5 1 5 3 5 2 3 4 4 1 3 1 5 5 1 2 2 1 3 1" +
+                " 5 1 2 2 1 5 1 3 3 2 1 3 2 5 1 1 2 3 5 5 4 3 1 3 3 1 5 4 2 3 4";
+        String[] integerStrings = mass.split(" ");
+        int[] integers = new int[integerStrings.length];
+        for (int i = 0; i < integers.length; i++) {
+            integers[i]=Integer.parseInt(integerStrings[i]);
+        }
+        int maxInt = 0;
+        for (int i = 0; i < integers.length-1; i++) {
+            if (integers[i]>maxInt){
+                maxInt = integers[i];
+            }
+        }
+        int[]counts = new int[maxInt+1];
+
+        for (int i = 0; i < integers.length; i++) {
+            for (int j = 1; j <= maxInt; j++) {
+                if (integers[i]==j){
+                    counts[j]++;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println(maxInt);
+        System.out.println(Arrays.toString(counts));
+    }
 }
