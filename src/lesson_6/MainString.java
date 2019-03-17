f8798be9615d6f20d95f988daab2ff9ddbbd74eb
fimakoff <fimakoff@gmail.com>
2019-03-17 16:20:48 +0300

    add & modified ex homework lesson 6

--- src/lesson_6/MainString.java
+++ src/lesson_6/MainString.java
@@ -1,4 +1,30 @@
 package lesson_6;
 
 public class MainString {
+    public static void main(String[] args) {
+        String s = "Предложение  один     Теперь предложение    два   Предложение   три";
+        System.out.println(s);
+        String s1 = s.replaceAll(" +", " ");
+        StringBuilder s2 = new StringBuilder(s1);
+
+        for (int i = 1; i < s1.length(); i++) {
+
+            if(s2.charAt(i) >= 'А' && s2.charAt(i) <= 'Я') {
+                s2.setCharAt(i-1, '.');
+            }
+
+        }
+
+        for (int i =1; i<s2.length();i++){
+
+            if (s2.charAt(i)=='.'){
+                s2.insert(i+1," ");
+            }
+        }
+
+        s2.append('.');
+        System.out.println(s2.toString());
+
+
+    }
 }
